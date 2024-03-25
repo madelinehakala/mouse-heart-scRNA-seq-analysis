aeb27f3a787c1511131b21a658ef2f842714f12f
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-03-25 15:08:56 -0500

    Create wrapper.py

---
+++ wrapper.py
@@ -0,0 +1,46 @@
+import argparse
+import sys
+import os
+import shutil
+
+def check_arg(args = None):
+	'''Parses command line arguments.'''
+	parser = argparse.ArgumentParser(description = 'Pipeline for scRNA-Seq analysis.')
+	parser.add_argument('-o', '--outputDirectory',
+		help = 'Directory where all outputed files and folders will be stored',
+		required = 'True'
+		)	
+	parser.add_argument('-l', '--logFileName',
+		help = 'Desired Log File Name',
+		required = 'True'
+		)
+	return parser.parse_args(args)
+
+def initializeOutputDirectory(directory):
+  '''Initializes a directory where all outputted files, folders, etc will be stored.'''
+  if os.path.exists(directory): # if the directory with that name already exists, remove it
+    shutil.rmtree(directory)
+  os.makedirs(directory) # then make the directory and move into it
+  os.chdir(directory)
+  
+def createLog(logFileName):
+  '''Creates an empty log file where run info will be stored.'''
+  logFile = open(logFileName, 'w')
+  return logFile
+
+def callMatrixReader(matrixReaderScript):
+  '''Function to call the sleuth script, which is an Rscript.'''
+  callingScript = f'Rscript {matrixReaderScript}'
+  os.system(callingScript)
+  logFile.write('Reading data in matrix format... COMPLETE')
+  logFile.write('\n------------------------------\n')
+  
+  
+# retrieving command line arguments and assigning to variables
+args = check_arg(sys.argv[1:])
+outputDir = args.outputDirectory
+logFileName = args.logFileName
+
+initializeOutputDirectory(outputDir)
+logFile = createLog(logFileName)
+matrixRead = callMatrixReader('../readMatrixData.R')
