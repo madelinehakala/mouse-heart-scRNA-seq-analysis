9ad50a28921d321f108a44fcbb4ac218ac0a163c
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-03-27 15:53:36 -0500

    Update seurat.R

--- seurat.R
+++ seurat.R
@@ -16,4 +16,18 @@ plot1 = FeatureScatter(zone2, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 = FeatureScatter(zone2, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot1 + plot2
 zone2 = subset(zone2, subset = nFeature_RNA > 1000 & nFeature_RNA < 4500)
-zone2 =  NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+zone2 =  NormalizeData(zone2, normalization.method = "LogNormalize", scale.factor = 10000)
+VlnPlot(zone2, features = c("Myh6", "Actc1"))
+
+
+## Zone 3 Analysis
+zone3 = CreateSeuratObject(counts = zone3.data, project = "Zone III", min.cells = 3, min.features = 200)
+zone3
+zone3[["percent.mt"]] = PercentageFeatureSet(zone3, pattern = "^MT-")
+VlnPlot(zone3, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+plot1 = FeatureScatter(zone3, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 = FeatureScatter(zone3, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+zone3 = subset(zone3, subset = nFeature_RNA > 1000 & nFeature_RNA < 4500)
+zone3 =  NormalizeData(zone3, normalization.method = "LogNormalize", scale.factor = 10000)
+VlnPlot(zone3, features = c("Myh6", "Actc1"))
