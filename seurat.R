c0ea9fb029621a58614a20f7927023b2ffcb14c5
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-03-27 15:43:36 -0500

    Update seurat.R

--- seurat.R
+++ seurat.R
@@ -2,11 +2,18 @@ library(dplyr)
 library(Seurat)
 library(patchwork)
 
+## Paths to data
 zone2.data = Read10X(data.dir = "/Users/madelinehakala/mouse-heart-scRNA-seq-analysis/dataZone2/")
 zone3.data = Read10X(data.dir = "/Users/madelinehakala/mouse-heart-scRNA-seq-analysis/dataZone3/")
 
+
+## Zone 2 Analysis
 zone2 = CreateSeuratObject(counts = zone2.data, project = "Zone II", min.cells = 3, min.features = 200)
 zone2
-
-zone3 = CreateSeuratObject(counts = zone3.data, project = "Zone III", min.cells = 3, min.features = 200)
-zone3
+zone2[["percent.mt"]] = PercentageFeatureSet(zone2, pattern = "^MT-")
+VlnPlot(zone2, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+plot1 = FeatureScatter(zone2, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 = FeatureScatter(zone2, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+zone2 = subset(zone2, subset = nFeature_RNA > 1000 & nFeature_RNA < 4500)
+zone2 =  NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
