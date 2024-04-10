e2d564ffa6eed44dfa516ec2f2eb1352a22642cf
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-04-09 23:20:01 -0500

    Update runExperiment.py

--- runExperiment.py
+++ runExperiment.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import os
 import shutil
+import csv
+import pandas as pd
 
 def check_arg(args = None):
 	'''Parses command line arguments.'''
@@ -22,6 +24,7 @@ def enterDirectory(directory):
 def createLog(logFileName):
   '''Creates an empty log file where run info will be stored.'''
   logFile = open(logFileName, 'w')
+  logFile.write('-------------------------------------------\n')
   return logFile
 
 def callSeurat(seuratScript):
@@ -29,8 +32,27 @@ def callSeurat(seuratScript):
   callingScript = f'Rscript {seuratScript}'
   logFile.write('Running Seurat... ')
   os.system(callingScript)
-  logFile.write('COMPLETE')
-  logFile.write('\n-------------------------------------------\n')
+  logFile.write('COMPLETE\n')
+  logFile.write('-------------------------------------------\n')
+
+def selectMuscleCells(seuratOutfile, filteredOutfileName, filterValueA, filterValueB):
+  expressionData = pd.read_csv(seuratOutfile)
+  logFile.write(f'{seuratOutfile} prior to filtering...\n\n')
+  logFile.write(f'{expressionData.head()}\n\n')
+  muscleContractionFile = open('muscle_contraction_genes.txt', 'r')
+  muscleContractionData = muscleContractionFile.read()
+  muscleContractionGeneList = muscleContractionData.split(',')
+  expressedMuscleContractionGeneList = []
+  for gene in muscleContractionGeneList:
+    if gene in expressionData.columns:
+      expressedMuscleContractionGeneList.append(gene)
+  expressionDataMuscleContractionGenes = expressionData[expressedMuscleContractionGeneList]
+  expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes['Myh6'] >= filterValueA]
+  expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes['Actc1'] >= filterValueB]
+  expressionDataMuscleContractionGenes.to_csv(filteredOutfileName, index = False)
+  logFile.write(f'{seuratOutfile} after filtering...\n\n')
+  logFile.write(f'{expressionDataMuscleContractionGenes.head()}\n')
+  logFile.write('-------------------------------------------\n')
   
   
 # retrieving command line arguments and assigning to variables
@@ -42,4 +64,6 @@ logFileName = args.logFileName
 enterDirectory(directory)
 logFile = createLog(logFileName)
 seurat = callSeurat('seurat.R')
+zone2MuscleCells = selectMuscleCells('zone2_expression_data.csv', 'zone2_expression_data_genes_of_interest.csv', 0.75, 3)
+zone3MuscleCells = selectMuscleCells('zone3_expression_data.csv', 'zone3_expression_data_genes_of_interest.csv', 0.75, 3)
 
