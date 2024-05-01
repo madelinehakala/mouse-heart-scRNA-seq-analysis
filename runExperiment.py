c0f119343c56a52ecf007b652e5093a96d466add
Madeline Hakala <117949834+madelinehakala@users.noreply.github.com>
2024-05-01 09:21:26 -0500

    Update runExperiment.py

--- runExperiment.py
+++ runExperiment.py
@@ -5,6 +5,7 @@ import shutil
 import csv
 import pandas as pd
 import seaborn as sns
+import subprocess
 
 def check_arg(args = None):
 	'''Parses command line arguments.'''
@@ -13,10 +14,38 @@ def check_arg(args = None):
 		help = 'Directory where data and scripts are stored',
 		required = 'True'
 		)	
+	parser.add_argument('-g', '--genesOfInterestFile',
+		help = 'File name for a text file that contains the genes of interest you would like to include in the final dataset',
+		required = 'True'
+		)	
+	parser.add_argument('-s', '--sampleLabel',
+		help = 'Provide a label for your samples (ex: heartZone2)',
+		required = 'True'
+		)	
 	parser.add_argument('-l', '--logFileName',
 		help = 'Desired Log File Name',
 		required = 'True'
 		)
+	parser.add_argument('-g1', '--markerGene1',
+		help = 'Marker gene 1 (required)',
+		required = 'False'
+		)
+	parser.add_argument('-g2', '--markerGene2',
+		help = 'Marker gene 2 (not a required argument)',
+		required = 'False'
+		)
+	parser.add_argument('-f', '--filteredOutfileName',
+		help = 'Desired Name for the Filtered Outfile (with selected cell population of interest)',
+		required = 'False'
+		)	
+	parser.add_argument('-p', '--plotFileName',
+		help = 'Desired Name for the Expression Plot File',
+		required = 'False'
+		)	
+	parser.add_argument('-n', '--normalizedOutfileName',
+		help = 'Desired Name for the final outfile with normalized data',
+		required = 'False'
+		)	
 	return parser.parse_args(args)
 
 def enterDirectory(directory):
@@ -30,31 +59,56 @@ def createLog(logFileName):
 
 def callSeurat(seuratScript):
   '''Function to call the Seurat script, which is an Rscript.'''
-  callingScript = f'Rscript {seuratScript}'
   logFile.write('Running Seurat... ')
-  os.system(callingScript)
+  subprocess.call(['Rscript', seuratScript, directory, sampleLabel, markerGene1, markerGene2])
   logFile.write('COMPLETE\n')
   logFile.write('-------------------------------------------\n')
+  seuratOutfile = f'{sampleLabel}_expression_data.csv'
+  return seuratOutfile
 
-def selectMuscleCells(seuratOutfile, filteredOutfileName, filterValueA, filterValueB):
+def selectCells(seuratOutfile, filteredOutfileName, genesOfInterest, markerGene1, markerGene2):
   '''Filters the outputed file from Seurat to include only muscle cells. Additionally, it selects only the columns with muscle contraction genes of interest. Outputs this data into a new csv.'''
-  expressionData = pd.read_csv(seuratOutfile)
-  logFile.write(f'{seuratOutfile} prior to filtering...\n\n')
-  logFile.write(f'{expressionData.head()}\n\n')
-  muscleContractionFile = open('muscle_contraction_genes.txt', 'r')
-  muscleContractionData = muscleContractionFile.read()
-  muscleContractionGeneList = muscleContractionData.split(',')
-  expressedMuscleContractionGeneList = []
-  for gene in muscleContractionGeneList:
-    if gene in expressionData.columns:
-      expressedMuscleContractionGeneList.append(gene)
-  expressionDataMuscleContractionGenes = expressionData[expressedMuscleContractionGeneList]
-  expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes['Myh6'] >= filterValueA]
-  expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes['Actc1'] >= filterValueB]
-  expressionDataMuscleContractionGenes.to_csv(filteredOutfileName, index = False)
-  logFile.write(f'{filteredOutfileName} after filtering...\n\n')
-  logFile.write(f'{expressionDataMuscleContractionGenes.head()}\n')
-  logFile.write('-------------------------------------------\n')
+  if markerGene2 == None:
+    print('View the Rplots.pdf file for expression visualizations of your marker gene of interest to assess minimum necessary expression cutoffs.')
+    filterValueA = float(input(f'Minimum Expression Level of {markerGene1}: '))
+    expressionData = pd.read_csv(seuratOutfile)
+    logFile.write(f'{seuratOutfile} prior to filtering...\n\n')
+    logFile.write(f'{expressionData.head()}\n\n')
+    logFile.write('-------------------------------------------\n')
+    muscleContractionFile = open(genesOfInterest, 'r')
+    muscleContractionData = muscleContractionFile.read()
+    muscleContractionGeneList = muscleContractionData.split(',')
+    expressedMuscleContractionGeneList = []
+    for gene in muscleContractionGeneList:
+      if gene in expressionData.columns:
+        expressedMuscleContractionGeneList.append(gene)
+    expressionDataMuscleContractionGenes = expressionData[expressedMuscleContractionGeneList]
+    expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes[markerGene1] >= filterValueA]
+    expressionDataMuscleContractionGenes.to_csv(filteredOutfileName, index = False)
+    logFile.write(f'{filteredOutfileName} after filtering...\n\n')
+    logFile.write(f'{expressionDataMuscleContractionGenes.head()}\n')
+    logFile.write('-------------------------------------------\n')
+  else:
+    print('View the Rplots.pdf file for expression visualizations of your marker genes of interest to assess necessary minimum expression cutoffs.')
+    filterValueA = float(input(f'Set Minimum Expression Level of {markerGene1}: '))
+    filterValueB = float(input(f'Set Minimum Expression Level of {markerGene2}: '))
+    expressionData = pd.read_csv(seuratOutfile)
+    logFile.write(f'{seuratOutfile} prior to filtering...\n\n')
+    logFile.write(f'{expressionData.head()}\n\n')
+    muscleContractionFile = open('muscle_contraction_genes.txt', 'r')
+    muscleContractionData = muscleContractionFile.read()
+    muscleContractionGeneList = muscleContractionData.split(',')
+    expressedMuscleContractionGeneList = []
+    for gene in muscleContractionGeneList:
+      if gene in expressionData.columns:
+        expressedMuscleContractionGeneList.append(gene)
+    expressionDataMuscleContractionGenes = expressionData[expressedMuscleContractionGeneList]
+    expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes[markerGene1] >= filterValueA]
+    expressionDataMuscleContractionGenes = expressionDataMuscleContractionGenes.loc[expressionDataMuscleContractionGenes[markerGene1] >= filterValueB]
+    expressionDataMuscleContractionGenes.to_csv(filteredOutfileName, index = False)
+    logFile.write(f'{filteredOutfileName} after filtering...\n\n')
+    logFile.write(f'{expressionDataMuscleContractionGenes.head()}\n')
+    logFile.write('-------------------------------------------\n')
   
 def visualizeExpressionData(filteredOutfile, plotFileName):
   '''Visualizes expression data.'''
@@ -66,7 +120,7 @@ def visualizeExpressionData(filteredOutfile, plotFileName):
   figure = boxplot.get_figure()
   figure.savefig(plotFileName)
   
-def normalizeExpressionData(filteredOutfile, outfileName):
+def normalizeExpressionData(filteredOutfile, normalizedOutfileName):
   '''Calculates the average expression level of each muscle marker gene and outputs to a new file.'''
   filteredExpressionData = pd.read_csv(filteredOutfile)
   for column in filteredExpressionData.columns:
@@ -79,24 +133,30 @@ def normalizeExpressionData(filteredOutfile, outfileName):
         filteredExpressionData[column] = values
     except TypeError:
       continue
-  filteredExpressionData.to_csv(outfileName, index = False)
-  logFile.write(f'{outfileName} after normalizing expression levels...\n\n')
+  filteredExpressionData.to_csv(normalizedOutfileName, index = False)
+  logFile.write(f'{normalizedOutfileName} after normalizing expression levels...\n\n')
   logFile.write(f'{filteredExpressionData.head()}\n')
   logFile.write('-------------------------------------------\n')
   
 # retrieving command line arguments and assigning to variables
 args = check_arg(sys.argv[1:])
 directory = args.directory
+genesOfInterest = genesOfInterestFile
+sampleLabel = args.sampleLabel
 logFileName = args.logFileName
+markerGene1 = args.markerGene1
+try:
+  markerGene2 = args.markerGene2
+except:
+  markerGene2 = None
+filteredOutfileName = args.filteredOutfileName
+plotFileName = args.plotFileName
+normalizedOutfileName = args.normalizedOutfileName
 
-enterDirectory(directory)
+# calling functions
 logFile = createLog(logFileName)
 seurat = callSeurat('seurat.R')
-zone2MuscleCells = selectMuscleCells('zone2_expression_data.csv', 'zone2_muscle_cell_expression_data.csv', 0.75, 3) # 0.75 is minimum expression level for Myh6
-zone3MuscleCells = selectMuscleCells('zone3_expression_data.csv', 'zone3_muscle_cell_expression_data.csv', 0.75, 3) # 3 is minimum expression level for Actc1
-zone2Visualization = visualizeExpressionData('zone2_muscle_cell_expression_data.csv', 'zone2_muscle_cell_expression_visualization.png')
-zone3Visualization = visualizeExpressionData('zone3_muscle_cell_expression_data.csv', 'zone3_muscle_cell_expression_visualization.png')
-zone2MuscleCellsNormalized = normalizeExpressionData('zone2_muscle_cell_expression_data.csv', 'zone2_muscle_cell_normalized_expression_data.csv')
-zone3MuscleCellsNormalized = normalizeExpressionData('zone3_muscle_cell_expression_data.csv', 'zone3_muscle_cell_normalized_expression_data.csv')
-zone2NormalizedVisualization = visualizeExpressionData('zone2_muscle_cell_normalized_expression_data.csv', 'zone2_muscle_cell_normalized_expression_visualization.png')
-zone3NormalizedVisualization = visualizeExpressionData('zone3_muscle_cell_normalized_expression_data.csv', 'zone3_muscle_cell_normalized_expression_visualization.png')
+cellsOfInterest = selectCells(seurat, filteredOutfileName, genesOfInterest, markerGene1, markerGene2)
+preNormalizationVisualization = visualizeExpressionData(filteredOutfileName, plotFileName)
+cellsOfInterestNormalized = normalizeExpressionData(filteredOutfileName, normalizedOutfileName)
+postNormalizationVisualization = visualizeExpressionData(normalizedOutfileName, f'normalized_{plotFileName}')
